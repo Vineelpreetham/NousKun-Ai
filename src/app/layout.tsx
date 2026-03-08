8713802fe11dd13f7514dc328b2b49f06bb08c5d
Vineelpreetham <preethamvineel544@gmail.com>
2026-03-08 17:29:04 +0530

    fix: move AiStrategistWidget outside LaunchScreen wrapper to prevent rendering locks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,8 +62,8 @@ export default function RootLayout({
               {children}
             </main>
           </SmoothScroll>
-          <AiStrategistWidget />
         </LaunchScreen>
+        <AiStrategistWidget />
       </body>
     </html>
   );
